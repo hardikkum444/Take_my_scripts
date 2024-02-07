b4a52793fd270c34c261b3f3dab636b4ac76ac22
Hardik Kumawat <114590291+hardikkum444@users.noreply.github.com>
2024-02-07 18:07:37 +0530

    added a colour theme

--- TerminalCoulourTheme.sh
+++ TerminalCoulourTheme.sh
@@ -29,3 +29,27 @@ PS1+=' \n\$ '
 
 # Or if you just want a space not a new line
 PS1+=' \$ '
+
+-------------------------------------------------------------------
+
+#(Orange blue colour theme)
+
+# Set terminal background to a very dark blue 
+echo -e "\033]11;#000022\007"
+
+# Set username color to light green
+PS1='\[\e[38;5;196;1m\]\u\[\e[0m\]'
+
+# Set hostname color to neon pink
+PS1+='@\[\e[38;5;207m\]\h\[\e[0m\]'
+
+# Set current directory color to bold purple
+PS1+=':\[\e[1;35m\]\w\[\e[0m\]'
+
+# Set normal input text color to orange -->
+
+#lighter orange 
+#echo -e "\033]10;#FFA500\007"
+
+#darker orange 
+echo -e "\033]10;#FF8000\007"
