e5834f8296be285b7f0a159a520e69bbc4abdeac
hardikkum444 <hardikkumawat444@gmail.com>
2024-11-24 12:45:29 +0530

    minute modifications in life make the most impactful changes

--- tmux_sesh.sh
+++ tmux_sesh.sh
@@ -2,14 +2,13 @@
 
 #!/bin/bash
 tmux new-session -d -s lock_in
-tmux rename-window -t lock_in:0 'musikcube'
-tmux send-keys -t lock_in:0 'musikcube' C-m
-tmux new-window -t lock_in:1 -n 'code'
-tmux new-window -t lock_in:2 -n 'run'
+tmux rename-window -t lock_in:1 'musikcube'
+tmux send-keys -t lock_in:1 'musikcube' C-m
+tmux new-window -t lock_in:2 -n 'code'
+tmux new-window -t lock_in:3 -n 'run'
 tmux attach-session -t lock_in
 
 : '
-
 ->> tmux - Scrolling
 <leader> -> <Ctrl>-b
 
@@ -21,6 +20,5 @@ q to exit copy-mode
 
 ## Enabling the mouse/trackpad:
 <Leader> ": set -g mouse on" enables scrolling with the mouse wheel
-
 '
 
